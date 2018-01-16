f50b867e14735dadd7fdf10af730fcad77ea6af0
George Higbie <gehigbie@gmail.com>
2018-01-16 15:10:07 +0000

    Add react component

--- src/app.js
+++ src/app.js
@@ -11,8 +11,26 @@ class IndecisonApp extends React.Component{
     }
 }
 
-class
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Indecison App</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
+}
 
-
+class Action extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
+}
 
 const appRoot = document.getElementById('react-container');
+ReactDOM.render(<IndecisonApp />, appRoot);
