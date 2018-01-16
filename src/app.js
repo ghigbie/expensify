ce59fd76006f3a0af78d4b3baa398249700055cd
George Higbie <gehigbie@gmail.com>
2018-01-16 15:07:33 +0000

    Add basic react components

---
+++ src/app.js
@@ -0,0 +1,18 @@
+class IndecisonApp extends React.Component{
+    render(){
+        return(
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
+    }
+}
+
+class
+
+
+
+const appRoot = document.getElementById('react-container');
