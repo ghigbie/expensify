58de9bc1ba3997ecaad938ea0a410a0a68e59e51
George Higbie <gehigbie@gmail.com>
2018-04-19 15:52:37 +0000

    Break out action component

---
+++ src/Components/Action.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Action = (props) => {
+    return(
+        <div>
+            <button
+                onClick={props.handlePickOption}
+                className="btn btn-default"
+                disabled={!props.hasOptions}>What should I do?</button>
+        </div>
+    );  
+};
+
+export default Action;
