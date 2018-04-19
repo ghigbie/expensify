9ff9ff31aa2d4604377ddddf7b5e0a6125068b18
George Higbie <gehigbie@gmail.com>
2018-04-19 18:42:57 +0000

    Remove commented code

---
+++ public/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _IndecisionApp = require('Components/IndecisionApp');
+
+var _IndecisionApp2 = _interopRequireDefault(_IndecisionApp);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var appRoot = document.getElementById('react-container');
+_reactDom2.default.render(_react2.default.createElement(_IndecisionApp2.default, null), appRoot);
