a34d27c59995acec5954e82d48259e9a5c534b6f
George Higbie <gehigbie@gmail.com>
2018-04-19 15:36:49 +0000

    Create sheell for new main component

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class IndecisionApp extends Component{
+    constructor(props){
+        super(props)
+        this.state = {}    
+    }
+    
+    render(){
+        return(
+            
+        );
+    }
+}
+
+
+export default IndecisionApp;
---
+++ src/components/Options.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Option from './Option/Option';
+
+const Options = (props) => {
+  return(
+        <div>
+            <h3>Options {props.options.length}</h3>
+            {props.options.length === 0 && <p> Please add an option to get started! </p>}
+            
+            {
+                props.options.mapp((option) => (
+                    <Option key={option}
+                            optionText={option}
+                            handleDeletionOption={props.handleDeletionOption} />))
+            }
+            
+            <button className="but btn-outline-danger"
+                    onClick={props.handleDeletionOption}>Remove All</button>
+        </div>
+    );  
+};
