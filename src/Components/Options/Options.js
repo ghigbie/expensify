cbce82f6e1c8b1456cca0fbb4af26f1cf162811e
George Higbie <gehigbie@gmail.com>
2018-04-20 14:43:24 +0000

    Simplify options component

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -2,23 +2,21 @@ import React from 'react';
 
 import Option from './Option/Option';
 
-const Options = (props) => {
-  return(
-        <div>
-            <h3>Options {props.options.length}</h3>
-            {props.options.length === 0 && <p> Please add an option to get started! </p>}
+const Options = (props) => (
+    <div>
+        <h3>Options {props.options.length}</h3>
+        {props.options.length === 0 && <p> Please add an option to get started! </p>}
             
-            {
-                props.options.map((option) => (
-                    <Option key={option}
-                            optionText={option}
-                            handleDeletionOption={props.handleDeletionOption} />))
-            }
+        {
+            props.options.map((option) => (
+                <Option key={option}
+                        optionText={option}
+                        handleDeletionOption={props.handleDeletionOption} />))
+        }
             
-            <button className="but btn-outline-danger"
-                    onClick={props.handleDeletionOption}>Remove All</button>
-        </div>
-    );  
-};
+        <button className="but btn-outline-danger"
+                onClick={props.handleDeletionOption}>Remove All</button>
+    </div>
+);  
 
 export default Options;
