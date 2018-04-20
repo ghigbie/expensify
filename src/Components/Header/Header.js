ae435643bf0ddddc185d37c57512988385fbde1d
George Higbie <gehigbie@gmail.com>
2018-04-20 14:40:46 +0000

    Refactor header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Header = (props) => {
-    return(
-        <div>
-            <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
-        </div>
-    );
-};
+const Header = (props) => (
+    <div>
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>
+    </div>
+);
 
 export default Header;
