74e480df748a64bf25d8f21138f483bcbeeb2939
George Higbie <gehigbie@gmail.com>
2018-04-20 14:44:14 +0000

    Simplify action component

--- src/Components/Action/Action.js
+++ src/Components/Action/Action.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Action = (props) => {
-    return(
-        <div>
-            <button
-                onClick={props.handlePickOption}
-                className="btn btn-default"
-                disabled={!props.hasOptions}>What should I do?</button>
-        </div>
-    );  
-};
+const Action = (props) => (
+    <div>
+        <button
+            onClick={props.handlePickOption}
+            className="btn btn-default"
+            disabled={!props.hasOptions}>What should I do?</button>
+    </div>
+);  
 
 export default Action;
