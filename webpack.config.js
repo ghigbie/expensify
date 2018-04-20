62784948c1e0470e26cdce56d8f2d0040796fbbc
George Higbie <gehigbie@gmail.com>
2018-04-20 14:47:57 +0000

    Modify webpack contentbase

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,6 @@ module.exports = {
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contenBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public')
     }
 };
