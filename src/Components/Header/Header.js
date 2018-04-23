d976aa2a4cea648a1d37e82eb12eea4de17cd5d3
George Higbie <gehigbie@gmail.com>
2018-04-23 17:00:31 +0000

    Edit container values

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,8 +3,10 @@ import './Header.scss';
 
 const Header = (props) => (
     <div className="header">
-        <h1 className="header__title">{props.title}</h1>
-        <h2 className="header__subtitle">{props.subtitle}</h2>
+        <div className="container">
+            <h1 className="header__title">{props.title}</h1>
+            <h2 className="header__subtitle">{props.subtitle}</h2>
+        </div>
     </div>
 );
 
