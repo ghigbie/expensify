22f7642af67649ca22ce2b6f0af36b894f28c6e0
George Higbie <gehigbie@gmail.com>
2018-04-23 17:06:24 +0000

    Apply container class to mid elements

--- src/Components/IndecisionApp/IndecisionApp.js
+++ src/Components/IndecisionApp/IndecisionApp.js
@@ -94,14 +94,16 @@ class IndecisionApp extends Component{
                     title={title}
                     subtitle={subtitle} />
                     
-                <Action hasOptions={this.state.options.length > 0} 
-                        handlePickOption={this.handlePickOption}/>
-                        
-                <Options options={this.state.options} 
-                         handleDeleteOptions={this.handleDeleteOptions}
-                         handleDeleteOption={this.handleDeleteOption}/>
-                         
-                <AddOption handleAddOption={this.handleAddOption}/>
+                <div className="container">
+                    <Action hasOptions={this.state.options.length > 0} 
+                            handlePickOption={this.handlePickOption}/>
+                            
+                    <Options options={this.state.options} 
+                             handleDeleteOptions={this.handleDeleteOptions}
+                             handleDeleteOption={this.handleDeleteOption}/>
+                             
+                    <AddOption handleAddOption={this.handleAddOption}/>
+                </div>
                 
                 <OptionModal selectedOption={this.state.selectedOption}
                              handleClearSelectedOption={this.handleClearSelectedOption}/>
