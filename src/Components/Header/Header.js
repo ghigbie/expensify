96ddb3394ffe059221d44f4ca7d766d2f6a602a6
George Higbie <gehigbie@gmail.com>
2018-04-23 15:28:01 +0000

    Add styles to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Header.scss';
 
 const Header = (props) => (
-    <div>
-        <h1>{props.title}</h1>
-        <h2>{props.subtitle}</h2>
+    <div className="header">
+        <h1 className="header__title">{props.title}</h1>
+        <h2 className="header__subtitle">{props.subtitle}</h2>
     </div>
 );
 
