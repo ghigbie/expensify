a44fbb9385d310c2510e2a86fe48ac461dae364e
George Higbie <gehigbie@gmail.com>
2018-04-24 14:50:52 +0000

    Edit handle delete option function

--- src/Components/Options/Options.js
+++ src/Components/Options/Options.js
@@ -7,7 +7,7 @@ const Options = (props) => (
         <div className="widget-header">
             <h3 className="widget-header__title">Your Options</h3>
             <button className="button button--link"
-                    onClick={props.handleDeletionOption}>Remove All</button>
+                    onClick={props.handleDeletionOptions}>Remove All</button>
         </div>
         {props.options.length === 0 && <p> Please add an option to get started! </p>}
             
@@ -16,7 +16,7 @@ const Options = (props) => (
                 <Option key={option}
                         optionText={option}
                         count={index +1}
-                        handleDeletionOption={props.handleDeletionOption} />))
+                        handleDeleteOption={props.handleDeleteOption} />))
         }
     </div>
 );  
