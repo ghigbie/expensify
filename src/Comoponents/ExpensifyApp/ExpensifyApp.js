c9409344ccc7285b0f01dce3d00e3307fbe8270e
George Higbie <gehigbie@gmail.com>
2018-05-26 03:39:23 +0000

    Add routes and blank components

--- src/Comoponents/ExpensifyApp/ExpensifyApp.js
+++ src/Comoponents/ExpensifyApp/ExpensifyApp.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import {} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+//Routes
+import ExpenseDashboardPage from './../Routes/ExpenseDashboardPage/ExpenseDashboardPage';
+import AddExpensePage from './../Routes/AddExpensePage/AddExpensePage';
+import EditExpensePage from './../Routes/EditExpensePage/EditExpensePage';
+import HelpPage from './../Routes/HelpPage/HelpPage';
+import NotFoundPage from './../NotFoundPage/NotFoundPage';
+
 
 class ExpensifyApp extends Component{
     render(){
         return(
             <div>
                 <h1>Expensify App</h1>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" component={ExpenseDashboardPage} />
+                        <Route path="/create" component={AddExpensePage} />
+                        <Route path="/edit" component={EditExpensePage} />
+                        <Route path="/help" component={HelpPage} />
+                        <route component={NotFoundPage} />
+                    </Switch>
+                </BrowserRouter>
             </div>
         );
         
