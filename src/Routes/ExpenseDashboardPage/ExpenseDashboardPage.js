e006ee0b077541cbdb07abbf69bb8d191242cf0a
George Higbie <gehigbie@gmail.com>
2018-05-26 03:43:29 +0000

    Add expense dashboard page

---
+++ src/Routes/ExpenseDashboardPage/ExpenseDashboardPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ExpenseDashboardPage extends Component{
+    reder(){
+        return(
+            <div>
+                <h1>Expense Dashborad Page</h1>
+            </div>
+        );
+    }
+}
+
+export default ExpenseDashboardPage;
