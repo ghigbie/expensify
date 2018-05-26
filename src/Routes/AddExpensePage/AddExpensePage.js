3f3f04a816d396dcf471173fd3d537e207a97579
George Higbie <gehigbie@gmail.com>
2018-05-26 03:42:32 +0000

    Add two components code

---
+++ src/Routes/AddExpensePage/AddExpensePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddExpensePage extends Component{
+    reder(){
+        return(
+            <div>
+                <h1>Add Expense Page</h1>
+            </div>
+        );
+    }
+}
+
+export default AddExpensePage;
---
+++ src/Routes/EditExpensePage/EditExpensePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EditExpensePage extends Component{
+    reder(){
+        return(
+            <div>
+                <h1>Edit Expense Page</h1>
+            </div>
+        );
+    }
+}
+
+export default EditExpensePage;
