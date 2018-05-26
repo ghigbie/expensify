06ec1a12dfc5d218d2a298dfab67b910a6e9ba29
George Higbie <gehigbie@gmail.com>
2018-05-26 03:45:06 +0000

    Add not found page

---
+++ src/Routes/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NotFoundPage extends Component{
+    reder(){
+        return(
+            <div>
+                <h1>Not Found Page</h1>
+            </div>
+        );
+    }
+}
+
+export default NotFoundPage;
