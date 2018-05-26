8b8b0f675b58dc9f5365a3b90e24972b034377d2
George Higbie <gehigbie@gmail.com>
2018-05-26 03:44:08 +0000

    Add help page

---
+++ src/Routes/HelpPage/HelpPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HelpPage extends Component{
+    reder(){
+        return(
+            <div>
+                <h1>Help Page</h1>
+            </div>
+        );
+    }
+}
+
+export default HelpPage;
