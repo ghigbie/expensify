ad6096f59e38f3327ec386b6a58d330f0d8bb912
George Higbie <gehigbie@gmail.com>
2018-05-26 02:17:23 +0000

    Add new Expensify component

---
+++ src/Comoponents/ExpensifyApp/ExpensifyApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ExpensifyApp extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Expensify App</h1>
+            </div>
+        );
+        
+    }
+}
+
+export default ExpensifyApp;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './Components/IndecisionApp/IndecisionApp';
+import ExpensifyApp from './Components/ExpensifyApp/ExpensifyApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
