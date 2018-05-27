6c374af816aebacd1ab98956d2a930f7a8ae66d7
George Higbie <gehigbie@gmail.com>
2018-05-27 00:57:06 +0000

    Debug file structure

--- src/Components/ExpensifyApp/ExpensifyApp.js
+++ src/Components/ExpensifyApp/ExpensifyApp.js
@@ -1,34 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-//Components
-import Header from './../Header/Header';
-
-//Routes
-import ExpenseDashboardPage from './../../Routes/ExpenseDashboardPage/ExpenseDashboardPage';
-import AddExpensePage from './../../Routes/AddExpensePage/AddExpensePage';
-import EditExpensePage from './../../Routes/EditExpensePage/EditExpensePage';
-import HelpPage from './../../Routes/HelpPage/HelpPage';
-import NotFoundPage from './../../Routes/NotFoundPage/NotFoundPage';
-
+import AppRouter from './../../Routers/AppRouter';
 
 class ExpensifyApp extends Component{
     render(){
         return(
             <div>
                 <h1>Expensify App</h1>
-                <BrowserRouter>
-                    <div>
-                        <Header />
-                        <Switch>
-                            <Route path="/" exact={true} component={ExpenseDashboardPage} />
-                            <Route path="/create" component={AddExpensePage} />
-                            <Route path="/edit" component={EditExpensePage} />
-                            <Route path="/help" component={HelpPage} />
-                            <Route component={NotFoundPage} />
-                        </Switch>
-                    </div>
-                </BrowserRouter>
+                <AppRouter />
             </div>
         );
         
--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 //Components
-import Header from './../Header/Header';
+import Header from './../Components/Header/Header';
 
 //Routes
-import ExpenseDashboardPage from './../../Routes/ExpenseDashboardPage/ExpenseDashboardPage';
-import AddExpensePage from './../../Routes/AddExpensePage/AddExpensePage';
-import EditExpensePage from './../../Routes/EditExpensePage/EditExpensePage';
-import HelpPage from './../../Routes/HelpPage/HelpPage';
-import NotFoundPage from './../../Routes/NotFoundPage/NotFoundPage';
+import ExpenseDashboardPage from './../Routes/ExpenseDashboardPage/ExpenseDashboardPage';
+import AddExpensePage from './../Routes/AddExpensePage/AddExpensePage';
+import EditExpensePage from './../Routes/EditExpensePage/EditExpensePage';
+import HelpPage from './../Routes/HelpPage/HelpPage';
+import NotFoundPage from './../Routes/NotFoundPage/NotFoundPage';
 
 
 const AppRouter = () => 
